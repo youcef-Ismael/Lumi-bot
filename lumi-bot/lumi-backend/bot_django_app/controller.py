f6c2d912dd14368478f98275a5d611499e390349
toninagy <sm4le99tony@gmail.com>
2021-11-14 11:30:44 +0100

    Add methods to communicate with frontend

--- lumi-bot/lumi-backend/bot_django_app/controller.py
+++ lumi-bot/lumi-backend/bot_django_app/controller.py
@@ -15,12 +15,17 @@ class API:
     api_secret: str
 
 
+def start_bot(trade_type: TradeType, pair, quantity, paper):
+    # TODO validation logic
+    controller = Controller(trade_type, pair, quantity, paper)
+
+
 class Controller:
     """Class that communicates with the view (frontend) and trading bot model"""
 
     # TODO implement methods communicating with the frontend (Django) and, in turn, with the model
 
-    def __init__(self, trade_type: TradeType, order_type: OrderType, pair: tuple = ('BTC', 'USDT'), quantity=0.001, paper=True):
+    def __init__(self, trade_type: TradeType, pair: tuple = ('BTC', 'USDT'), quantity=0.001, paper=True):
         self.api_key = os.environ.get('binance_api')
         self.api_secret = os.environ.get('binance_secret')
         self.api_data = API(self.api_key, self.api_secret)
@@ -29,14 +34,11 @@ class Controller:
         if paper:
             self.client.API_URL = 'https://testnet.binance.vision/api'
         self.pair_str = pair[0] + pair[1]
-        self.trade_data = TradeData(trade_type, order_type, pair, self.pair_str, quantity)
+        self.trade_data = TradeData(trade_type, OrderType.MARKET, pair, self.pair_str, quantity)
         self.model = model.Model(self.trade_data, self.client)
 
     def get_asset_balance(self, asset):
         return self.client.get_asset_balance(asset=asset)
-
-    def create_order(self, symbol, side, order_type, quantity):  # might be redundant
-        self.client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
 
     def futures_account_transfer(self, asset, amount, f_type, timestamp=ctime()):
         self.client.futures_account_transfer(asset=asset, amount=amount, type=f_type, timestamp=timestamp)
