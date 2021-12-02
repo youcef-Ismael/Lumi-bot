47607deb972af52fdbaea8eb6ec0b09ca280efb4
toninagy <sm4le99tony@gmail.com>
2021-12-02 14:52:11 +0100

    Add sentiment based on fibo and further improve trading logic

--- bot_django_app/bot.py
+++ bot_django_app/bot.py
@@ -19,6 +19,11 @@ class OrderType(Enum):
     LIMIT = 2
 
 
+class Sentiment(Enum):
+    BEARISH = 1
+    BULLISH = 2
+
+
 @dataclass
 class TradeData:
     trade_type: TradeType
@@ -39,6 +44,8 @@ class Bot:
         self.loop = asyncio.get_event_loop()
         self.entered = False
         self.orders = []
+        self.fibo_lvls = []
+        self.sentiment = None
 
     def set_trade_data(self, trade_data):
         self.trade_data = trade_data
@@ -52,6 +59,7 @@ class Bot:
     def start(self):
         try:
             asyncio.ensure_future(self.populate_df(), loop=self.loop)
+            asyncio.ensure_future(self.refresh_fibo_lvls(), loop=self.loop)
             asyncio.ensure_future(self.start_trade(), loop=self.loop)
             self.loop.run_forever()
         except KeyboardInterrupt:
@@ -71,19 +79,19 @@ class Bot:
 
         while not self.stopped:
 
-            if not self.entered:
+            if (not self.entered or Sentiment.BULLISH) and (len(self.orders) < 3 or not all(order['side'] == 'BUY' for order in self.orders[-3:-1])):
                 if self.get_rsi(timeframe=Client.KLINE_INTERVAL_15MINUTE) < 15 or self.get_rsi(
                         timeframe=Client.KLINE_INTERVAL_1HOUR) < 15:
                     print('RSI is very low, strong buy signal')
                     self.__buy(self.trade_data.quantity)
 
-                elif self.get_rsi(timeframe=Client.KLINE_INTERVAL_1HOUR) < 30 or self.get_rsi(
-                        timeframe=Client.KLINE_INTERVAL_1DAY) < 30:
+                elif self.get_rsi(timeframe=Client.KLINE_INTERVAL_15MINUTE) < 30 or self.get_rsi(
+                        timeframe=Client.KLINE_INTERVAL_1HOUR) < 30:
                     print('RSI is low, buy signal, but the trend is being checked too')
                     self.trendfollow_buy()
 
-                elif self.get_rsi(timeframe=Client.KLINE_INTERVAL_1HOUR) < 50 or self.get_rsi(
-                        timeframe=Client.KLINE_INTERVAL_1DAY) < 50:
+                elif self.get_rsi(timeframe=Client.KLINE_INTERVAL_15MINUTE) < 50 or self.get_rsi(
+                        timeframe=Client.KLINE_INTERVAL_1HOUR) < 50:
                     if self.get_ema(timeframe=Client.KLINE_INTERVAL_1MINUTE) or self.get_ema(
                             timeframe=Client.KLINE_INTERVAL_1HOUR) > self.df.iloc[-1].Price:
                         print('RSI is neutral, but ema is lower than current price, buy signal')
@@ -94,15 +102,14 @@ class Bot:
                     print('RSI is high, but sma is much lower than current price, buy signal, but the trend is being '
                           'checked too')
                     self.trendfollow_buy()
-            elif self.get_rsi(timeframe=Client.KLINE_INTERVAL_1MINUTE) < 10:
-                self.trendfollow_buy()
-            else:
+
+            elif (self.entered or Sentiment.BEARISH) and (len(self.orders) < 3 or not all(order['side'] == 'SELL' for order in self.orders[-3:-1])):
+
                 self.threshold_sell(profit_threshold=0.1, loss_threshold=0.05)
 
                 if self.entered:
                     if self.get_rsi(timeframe=Client.KLINE_INTERVAL_1MINUTE) > 85 or self.get_rsi(
-                            timeframe=Client.KLINE_INTERVAL_1HOUR) > 85:  # no need to check for
-                        # earlier dates since we checked for that when we bought
+                            timeframe=Client.KLINE_INTERVAL_1HOUR) > 85:
                         print('RSI is very high, strong sell signal')
                         self.threshold_sell(profit_threshold=0.1, loss_threshold=0.1)
 
@@ -174,7 +181,8 @@ class Bot:
                 print(str(datetime.datetime.now()) + '\t-\tNo sell')
 
     def get_dataframe(self, timeframe=Client.KLINE_INTERVAL_1HOUR):
-        timestamp = self.api.client._get_earliest_valid_timestamp(self.trade_data.pair_str, '1d')
+        timestamp = self.api.client._get_earliest_valid_timestamp(self.trade_data.pair_str,
+                                                                  Client.KLINE_INTERVAL_1MONTH)
         klines = self.api.client.get_historical_klines(self.trade_data.pair_str, timeframe, timestamp, limit=1000)
         df = pd.DataFrame(klines)
         df = df.iloc[:, :6]
@@ -206,19 +214,20 @@ class Bot:
     def __get_highest_and_lowest_swing(self):
         highest_swing = -1
         lowest_swing = -1
-        df = self.get_dataframe()
+        df = self.get_dataframe(timeframe=Client.KLINE_INTERVAL_15MINUTE)
         for i in range(1, df.shape[0] - 1):
             if df['High'][i] > df['High'][i - 1] and df['High'][i] > df['High'][i + 1] and (
-                    highest_swing == -1 or df['High'][i] > df['High'][highest_swing]):
+                    highest_swing == -1 or (
+                    df['High'][highest_swing] < df['High'][i] < df['High'][highest_swing] * 1.05)):
                 highest_swing = i
             if df['Low'][i] < df['Low'][i - 1] and df['Low'][i] < df['Low'][i + 1] and (
-                    lowest_swing == -1 or df['Low'][i] < df['Low'][lowest_swing]):
+                    lowest_swing == -1 or (df['Low'][lowest_swing] * 0.95 < df['Low'][i] < df['Low'][lowest_swing])):
                 lowest_swing = i
         return highest_swing, lowest_swing
 
     def __get_max_and_min_lvl(self):
         hi, lo = self.__get_highest_and_lowest_swing()
-        df = self.get_dataframe()
+        df = self.get_dataframe(timeframe=Client.KLINE_INTERVAL_15MINUTE)
         max_level = df['High'][hi]
         min_level = df['Low'][lo]
         return max_level, min_level
@@ -226,7 +235,7 @@ class Bot:
     def get_fibonacci_ratio_lvls(self):
         max_level, min_level = self.__get_max_and_min_lvl()
         highest_swing, lowest_swing = self.__get_highest_and_lowest_swing()
-        ratios = [0.236, 0.382, 0.618, 0.786]
+        ratios = [0, 0.236, 0.382, 0.618, 0.786, 1]
         levels = []
         for ratio in ratios:
             if highest_swing > lowest_swing:  # Uptrend
@@ -234,3 +243,12 @@ class Bot:
             else:  # Downtrend
                 levels.append(min_level + (max_level - min_level) * ratio)
         return levels
+
+    async def refresh_fibo_lvls(self):
+        while not self.stopped:
+            self.fibo_lvls = self.get_fibonacci_ratio_lvls()
+            if self.fibo_lvls[0] > self.fibo_lvls[-1]:
+                self.sentiment = Sentiment.BEARISH
+            else:
+                self.sentiment = Sentiment.BULLISH
+            await asyncio.sleep(900)
